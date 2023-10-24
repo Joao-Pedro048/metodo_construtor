c16bd014359f098c077c4ee1aa3f64a298125bfa
Joao-Pedro048 <soncinjoao@gmail.com>
2023-10-24 13:42:34 +0000

    salvo

---
+++ Carro.java
@@ -0,0 +1,24 @@
+public class Carro{
+    private String cor;
+    private String modelo;
+    private int aro;
+    private double valorCarro;
+
+    public Carro(){
+
+    }
+    public Carro(int aro){
+        this.aro=aro;
+    }
+    public Carro(int aro, String modelo){
+        this.aro=aro;
+        this.modelo=modelo;
+    }
+    public Carro(int aro, String modelo,double valorCarro, String cor ){
+        this.aro=aro;
+        this.modelo=modelo;
+        this.valorCarro=valorCarro;
+        this.cor=cor;
+    }
+    
+}

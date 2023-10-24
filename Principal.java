08926e1179d51cae323b9e517aafd4379db4f629
Joao-Pedro048 <soncinjoao@gmail.com>
2023-10-24 14:25:52 +0000

    salvo

--- Principal.java
+++ Principal.java
@@ -2,6 +2,12 @@ public class Principal {
     
     public static void main(String args){
         
+        Carro car = new Carro();
+        Carro carAro = new Carro(14);
+        Carro carTresParametros = new
+        Carro(14, "HB 20 2021 sense");
+        Carro carQuatroParametros = 
+        new Carro();
         
     }
 }
